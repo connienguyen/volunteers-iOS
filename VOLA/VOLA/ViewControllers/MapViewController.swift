5b9fa3882cb4c4818e2b0be039afbf5c8ae804a1
connie <conniednguyen@gmail.com>
2017-07-31 13:21:04 +0200

    Set MapViewController as delegate for locationManager

--- VOLA/VOLA/ViewControllers/MapViewController.swift
+++ VOLA/VOLA/ViewControllers/MapViewController.swift
@@ -24,6 +24,7 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
 
         // Set up location manager and map view
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
